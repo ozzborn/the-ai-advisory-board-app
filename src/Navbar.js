036004a21a346a61bbbf7078211e4c4ecaae9bd9
ozzborn <ozzborn1987@gmail.com>
2025-10-21 10:43:50 -0400

    feat: Added missing NavBar component file.

---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NavBar() {
+  return (
+    <nav style={{ padding: '10px', backgroundColor: '#333', color: 'white' }}>
+      <h1>Main Navigation Bar</h1>
+    </nav>
+  );
+}
+
+export default NavBar;
